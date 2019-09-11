789de52ef75568c34f924f831af5ca8378eb5658
Scotty Skidmore <scotty.skidmore@gmail.com>
2019-09-11 17:11:51 +1000

    added commands to post_gen

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,6 +1,8 @@
 echo "Running post_gen_project"
 
 # todo set up git repo
-git 
+git init
+git add .
+git commit "init commit"
 # todo init commit
 # todo set up virtual environment for python project
