2ab018a3da6e7fa0af47fd229b55db5402302b0d
Scotty Skidmore <scotty.skidmore@gmail.com>
2019-09-11 17:50:25 +1000

    added cookiecutter variables to R file

---
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_file_name}}.R
@@ -0,0 +1,19 @@
+#{{cookiecutter.project_short_description}}
+#
+# {{cookiecutter.project_file_name}}
+# Docstring long (multiline) description
+#
+#
+# author = {{cookiecutter.full_name}}
+# created = {{cookiecutter.date}}
+# copyright = "Copyright 2019, CHE Proximity"
+# credits = ["{{cookiecutter.full_name}}", ]# List of people who worked on the project
+#
+# version = {{cookiecutter.version}}
+# maintainer = {{cookiecutter.full_name}}
+# email = {{cookiecutter.email}}
+# status = Development # Development, Prototype, or Production
+
+
+
+
--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_file_name}}.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_file_name}}.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 """{{cookiecutter.project_short_description}}
 
