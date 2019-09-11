16658a86f63c0ca4bda53d7f82fb62fc30d421c7
Scotty Skidmore <scotty.skidmore@gmail.com>
2019-09-11 17:40:24 +1000

    added r proj files

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,8 +1,6 @@
 echo "Running post_gen_project"
 
-# todo set up git repo
 git init
 git add .
 git commit -m "init commit"
-# todo init commit
 # todo set up virtual environment for python project
---
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}.R
@@ -0,0 +1,18 @@
+# Doc string short (single line) description 
+#
+# filename
+# Docstring long (multiline) description
+#
+#
+# author = # Your Name
+# created = # Todays Date YYYY-MM-DD
+# copyright = "Copyright 2019, CHE Proximity"
+# credits = ["", ]# List of people who worked on the project
+#
+# license = GPL
+# version = 0.0.1
+# maintainer = # Your name or the current maintainer
+# email = # Email Address of the current maintainer
+# status = Development # Development, Prototype, or Production
+
+
