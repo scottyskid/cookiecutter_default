940788159347b0eaca74171851fc887fc39f50e9
Scotty Skidmore <scotty.skidmore@gmail.com>
2019-10-04 15:35:16 +1000

    updated base dir

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -4,13 +4,13 @@ import sys
 
 
 if __name__ == "__main__":
-    BASE_DIR = Path(__file__).resolve().parent.parent
+    SRC_DIR = Path('src')
 {%- if cookiecutter.project_type == 'python' %}
-    os.remove(BASE_DIR / '{{ cookiecutter.project_slug }}' / '{{cookiecutter.project_file_name}}.R')
-    os.remove(BASE_DIR / '{{ cookiecutter.project_slug }}' / 'globals.R')
+    os.remove(SRC_DIR / '{{ cookiecutter.project_slug }}' / '{{cookiecutter.project_file_name}}.R')
+    os.remove(SRC_DIR / '{{ cookiecutter.project_slug }}' / 'globals.R')
 {%- elif  cookiecutter.project_type == 'r' %}
-    os.remove(BASE_DIR / '{{ cookiecutter.project_slug }}' / '{{cookiecutter.project_file_name}}.py')
-    os.remove(BASE_DIR / '{{ cookiecutter.project_slug }}' / 'globals.py')
+    os.remove(SRC_DIR / '{{ cookiecutter.project_slug }}' / '{{cookiecutter.project_file_name}}.py')
+    os.remove(SRC_DIR / '{{ cookiecutter.project_slug }}' / 'globals.py')
 {%- endif %}
     print("running git init")
     os.system('git init')
