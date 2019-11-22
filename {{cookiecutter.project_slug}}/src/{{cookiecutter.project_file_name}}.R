84877af0b941954d225fe00aeda2e0509e12601f
Scotty Skidmore <scotty.skidmore@gmail.com>
2019-11-22 14:59:02 +1100

    updated R default file

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_file_name}}.R
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_file_name}}.R
@@ -14,23 +14,41 @@
 # email = {{cookiecutter.email}}
 # status = Development # Development, Prototype, or Production
 
-
-if(!require(odbc))
-{install.packages("odbc") }
-if(!require(configr))
-{install.packages("configr") }
-if(!require(aws.s3))
-{install.packages("aws.s3") }
-if (!require(here))
-{install.packages("here")}
-
-
+pacman::p_load(configr,
+               odbc,
+               aws.s3,
+               here,
+               update= FALSE ,install=TRUE)
+​
 #clear all variables
-
-
+​
 rm(list = ls(all.names = TRUE))
+# store setiing file paths and read files
+​
+p_user_setting_loc = file.path(gsub("\\\\","/",as.character(Sys.getenv(x=c('USERPROFILE'),names=TRUE))),"user_settings.config",fsep = "/")
+user_config = read.config(p_user_setting_loc, file.type = 'ini')
+​
+p_project_setting_loc = file.path(here(),"project_settings.config",fsep="/")
+project_config = read.config(p_project_setting_loc, file.type = 'ini')
+​
+# aws_chepdata_con = odbc::dbConnect(odbc(),
+#                                    Driver = user_config$AWS_MSSQL_CHEPDATADB$DRIVER,
+#                                    Server = user_config$AWS_MSSQL_CHEPDATADB$ENDPOINT,
+#                                    Database = user_config$AWS_MSSQL_CHEPDATADB$DATABASE,
+#                                    UID = user_config$AWS_MSSQL_CHEPDATADB$USERNAME,
+#                                    PWD = user_config$AWS_MSSQL_CHEPDATADB$PASSWORD,
+#                                    Port = as.numeric(user_config$AWS_MSSQL_CHEPDATADB$PORT))
+# 
+# odbc::dbDisconnect(aws_chepdata_con)
+#
+# sam_ce <- aws.s3::s3read_using(FUN = read.csv,
+#                                bucket = <bucket name>,
+#                                object = <>)
 
-source(globals.R)
+#  aws.s3::s3write_using(FUN = write.csv,
+# 							x=df,
+#                           bucket = <bucket name>,
+#                           object = <>)
 
 
 
